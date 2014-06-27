e0e97ff2c9a2be4add7b97dcb0c2c983e523dcbc
Dries Desmet <dries@urga.be>
2014-06-27 16:35:21 +0200

    Make it a module

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name='pythonsite-dotenv',
-    url="www.wiezebier.be",
+    url="https://github.com/urga/pythonsite-dotenv",
     version='0.1dev',
-    install_requires=['django-dotenv'],
+    install_requires=['django-dotenv', ],
     description="Extend environment when invoking python",
     author="Dries Desmet",
     author_email="dries@urga.be",
     zip_safe=False,
-    py_modules=['sitecustomize'],
+    packages=['sitecustomize', ],
 )
--- sitecustomize/__init__.py
+++ sitecustomize/__init__.py
@@ -1,6 +1,7 @@
 import dotenv
 import sys
 import os
+
 dotenvpath = os.path.join(os.path.dirname(os.path.dirname(sys.executable)), ".env")
 if os.path.exists(dotenvpath):
     dotenv.read_dotenv(dotenvpath)
